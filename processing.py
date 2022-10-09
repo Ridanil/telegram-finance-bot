f3cafc1bd733a6811d5193ab7d1ece96f78b9726
Ridanil <94567068+Ridanil@users.noreply.github.com>
2022-10-09 23:58:29 +0300

    Add files via upload

--- processing.py
+++ processing.py
@@ -4,6 +4,9 @@ import pytz
 import re
 import quikstart
 
+import db
+
+
 class Message(NamedTuple):
     "Распаршенное сообщение"
     amount: int
@@ -16,12 +19,32 @@ class Expense(NamedTuple):
     category_name: str
 
 def add_expens(raw_message: str):
-	parsed_message = parsing(raw_message)
-	add_in_to_gs = quikstart.add_into_gs(parsed_message.amount, parsed_message.category_text)
-	return Expense(id=None, amount=parsed_message.amount, category_name=parsed_message.category_text)
+    parsed_message = parsing(raw_message)
+    add_in_to_gs = quikstart.add_into_gs(
+        parsed_message.amount,
+        parsed_message.category_text
+        )
+    insert_in_db = db.insert("expenses", {
+        "crete_date": _get_now_formated_datetime(),
+        "amount": parsed_message.amount,
+        "category_name": parsed_message.category_text
+        })
+    return Expense(id=None, amount=parsed_message.amount, category_name=parsed_message.category_text)
+
 
 def parsing(raw_message: str) -> Message:
     parsed_msg = re.match(r"([\d ]+) (.*)", raw_message)
     amount = int(parsed_msg.group(1))
     category_text = str(parsed_msg.group(2)).strip()
     return Message(amount=amount, category_text=category_text)
+
+def _get_now_datetime() -> datetime.datetime:
+    """Возвращает сегодняшний datetime с учетом временной зоны"""
+    tz = pytz.timezone("Europe/Moscow")
+    now = datetime.datetime.now(tz)
+    return now
+
+
+def _get_now_formated_datetime() -> str:
+    """Возвращает сегодняшнюю дату время строкой для БД"""
+    return _get_now_datetime().strftime("%Y-%m-%d %H:%M:%S")
