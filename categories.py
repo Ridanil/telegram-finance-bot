4d1f11238713ae1e5382e79696a2423a733e8dcc
Ridanil <94567068+Ridanil@users.noreply.github.com>
2023-01-19 20:48:34 +0300

    Add files via upload

---
+++ categories.py
@@ -0,0 +1,35 @@
+"""Работает с категориями"""
+import json
+import exceptions
+
+
+def get_category(text_for_prepare):
+    """Определяет категорию в которую необходимо записать расход"""
+    global data_from_json
+    with open("categories.json", "r", encoding="utf-8") as categ_data:
+        data_from_json = json.load(categ_data)
+    try:    
+        result = next(key for key in data_from_json
+                    if text_for_prepare in data_from_json[key])
+    except StopIteration:
+        raise exceptions.NoSuchCategory("Укажите категорию")
+    return result
+
+
+def update_categories_json(text_for_prepare, ask_category): # TODO what it must return? Maybe "PASS"?
+    """Если на основании нового расхода не удалось определить категорию,
+    она вводится c помощью кнопки а расход добавляется в алиасы"""    
+    try:
+        data_from_json[ask_category].append(text_for_prepare)
+        with open("categories.json", "w", encoding="utf-8") as categ:
+             json.dump(data_from_json, categ, indent=2)
+    except KeyError:
+        print(f"The {ask_category} is not a category") 
+    return ask_category
+
+def get_list_of_category():
+    """Возвращает список категорий"""
+    with open("categories.json", "r", encoding="utf-8") as categ_data:
+        data_from_json = json.load(categ_data)
+    list_of_category = [key for key in data_from_json]
+    return list_of_category
--- processing.py
+++ processing.py
@@ -12,12 +12,14 @@ class Message(NamedTuple):
     "Распаршенное сообщение"
     amount: int
     message_text: str
+    date: Optional[str]
 
 class Expense(NamedTuple):
     "Структура расхода"
     id: Optional[int]
     amount: int
-    category_name: str
+    category_name: Optional[str]
+    raw_text: Optional[str]
 
 
 class Earn(NamedTuple):
@@ -26,16 +28,19 @@ class Earn(NamedTuple):
     category_name: str
 
 
-def add_expense(amount: int, category: str, expense: str):
-    quikstart.add_into_gs(amount, category)
+cursor = db.get_cursor()
+
+
+def add_expense(amount: int, category: str, expense: str, date: str):
+    quikstart.add_into_gs(amount, category, date)
     db.insert("expenses", {
-        "create_date": _get_now_formated_datetime(),
+        "create_date": _get_now_formated_datetime(date),
         "amount": amount,
         "category_name": category,
         "raw_text": expense
         })
     db.update_budget(db.get_budget() - amount)
-    return Expense(id=None, amount=amount, category_name=category)
+    return Expense(id=None, amount=amount, category_name=category, raw_text=None)
 
 
 def add_income(amount: int, message_text: str):
@@ -52,18 +57,21 @@ def add_income(amount: int, message_text: str):
 
 def parsing(raw_message: str) -> Message:
     """Парсит входящее сообщение"""
-    parsed_msg = re.match(r"\A\+?(\d+)(\D+)", raw_message)
+    parsed_msg = re.match(r"\A\+?(\d+)(\D+)(\d{1,2}-\d{1,2})?", raw_message)
     if not parsed_msg or not parsed_msg.group(0) \
         or not parsed_msg.group(1) or not parsed_msg.group(2):
         raise exceptions.NotCorrectMessage("Неверно сообщение. Сначала цифры-(сколько) потом буквы-(на что)")
     amount = int(parsed_msg.group(1))
     message_text = str(parsed_msg.group(2)).strip()
-    return Message(amount=amount, message_text=message_text)
+    if parsed_msg.group(3):
+        date = str(parsed_msg.group(3))
+    else:
+        date = None
+    return Message(amount=amount, message_text=message_text, date=date)
 
 
 def get_today_statistics() -> str:
     """Возвращает строкой статистику расходов за сегодня"""
-    cursor = db.get_cursor()
     cursor.execute("select sum(amount)"
                    "from expenses where date(create_date)=date('now', 'localtime')")
     result = cursor.fetchone()
@@ -79,7 +87,6 @@ def get_month_statistic(category: str) -> str:
     now = _get_now_datetime()
     today = int(f'{now.day:02d}')
     first_day_of_month = f'{now.year:04d}-{now.month:02d}-01'
-    cursor = db.get_cursor()
     cursor.execute(db.month_statistic_query, (first_day_of_month, category))
     result = cursor.fetchone()
     if not result[0]:
@@ -94,12 +101,37 @@ def get_earn_statistic(): #TODO сделать коректный текст в 
     """Возвращает статистику прхода за месяц"""
     now = _get_now_datetime()
     first_day_of_month = f'{now.year:04d}-{now.month:02d}-01'
-    cursor = db.get_cursor()
     cursor.execute(db.month_earn_query, (first_day_of_month,))
     result = cursor.fetchall()
     earn = [Earn(amount=res[0], category_name=res[1]) for res in result]
     return earn
 
+
+def return_last_expenses():
+    """Возврашает 10 последних записей"""
+    cursor.execute(db.last_expenses_query)
+    rows = cursor.fetchall()
+    last_expenses = [Expense(id=row[0], amount=row[1],
+                             raw_text=row[2], category_name=None) for row in rows]
+    return last_expenses
+
+
+def delete_expense(row_id: int) -> None:
+    """Удаляет сообщение по его идентификатору"""
+    cursor.execute(db.amount_and_category_query, (row_id,))
+    rows = cursor.fetchone()
+    date = rows[2][8:10]
+    quikstart.add_into_gs(-abs(int(rows[0])), rows[1], int(date))
+    db.delete("expenses", row_id)
+
+
+def change_expense(row_id: int, new_value: int) -> None:
+    """Изменяет запись расхода по его идентификатору"""
+    cursor.execute(db.amount_and_category_query, (row_id,))
+    rows = cursor.fetchone()
+    date = rows[2][8:10]
+    quikstart.add_into_gs(new_value-rows[0], rows[1], int(date))
+    db.change("expenses", row_id, new_value)
 
 def _get_now_datetime() -> datetime.datetime:
     """Возвращает сегодняшний datetime с учетом временной зоны"""
@@ -108,7 +140,13 @@ def _get_now_datetime() -> datetime.datetime:
     return now
 
 
-def _get_now_formated_datetime() -> str:
+def _get_now_formated_datetime(specified_date=None):
     """Возвращает сегодняшнюю дату время строкой для БД"""
-    return _get_now_datetime().strftime("%Y-%m-%d %H:%M:%S")
+    if specified_date is None:
+        date = _get_now_datetime().strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        date = (str(_get_now_datetime().year)+"-"
+                + str(specified_date) + " "
+                + _get_now_datetime().strftime("%H:%M:%S"))
+    return date
 
