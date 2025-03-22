8d2d53aa73f3d95e7e14fd6b0ccacf58ade14459
Ridanil <ridanil59@gmail.com>
2025-03-22 16:48:22 +0300

    small refactoring

--- categories.py
+++ categories.py
@@ -18,7 +18,7 @@ def get_category(text_for_prepare):
 
 def update_categories_json(text_for_prepare, ask_category): # TODO what it must return? Maybe "PASS"?
     """Если на основании нового расхода не удалось определить категорию,
-    она вводится c помощью кнопки а расход добавляется в алиасы"""    
+    она вводится c помощью кнопки, а расход добавляется в алиасы"""
     try:
         data_from_json[ask_category].append(text_for_prepare)
         with open("categories/categories.json", "w", encoding="utf-8") as categ:
@@ -27,7 +27,7 @@ def update_categories_json(text_for_prepare, ask_category): # TODO what it must 
         print(f"The {ask_category} is not a category") 
     return ask_category
 
-def get_list_of_category():
+def get_list_of_category() -> list:
     """Возвращает список категорий"""
     with open("categories/categories.json", "r", encoding="utf-8") as categ_data:
         data_from_json = json.load(categ_data)
